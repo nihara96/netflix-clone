bdc6a6c264c921fcdcd2d601a8d0db836d329828
nihara <nbmayurawasala@gmail.com>
2023-03-19 11:09:59 +0530

    recoil

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,9 @@ import requests from "@/utils/requests";
 import { Movie } from "@/typings";
 import Row from "@/components/Row";
 import useAuth from "@/hooks/useAuth";
+import { useRecoilValue } from "recoil";
+import { modalState } from "@/atoms/modalAtom";
+import Modal from "@/components/Modal";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -29,10 +32,12 @@ export default function Home({
 }: Props) {
   const { loading } = useAuth();
 
+  const showModal = useRecoilValue(modalState);
+
   if (loading) {
     return null;
   }
-  
+
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -54,6 +59,7 @@ export default function Home({
           <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
+      {showModal && <Modal/>} 
     </div>
   );
 }
